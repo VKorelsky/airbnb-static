51fbafc556c01f462b668057f2de6b02bcd7a5d5
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-08-15 22:46:11 +0200

    clean up

--- config.rb
+++ config.rb
@@ -30,6 +30,9 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+["johny", "kanye", "jean"].each do |name|
+  proxy "/flats/#{name}.html", "/template.html", :locals => { :owner => name }, :ignore => true
+end
 
 ###
 # Helpers
@@ -39,9 +42,9 @@ activate :autoprefixer
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+   activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
